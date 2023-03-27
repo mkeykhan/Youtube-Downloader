859e950a15970483ccc90576f44098c7ef7b5d62
mkeykhan <xixkhan@gmail.com>
2023-03-27 09:43:08 +0500

    Static files issue

--- Kaleem/urls.py
+++ Kaleem/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from Kaleem import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 from django.views.static import serve
@@ -28,3 +29,6 @@ urlpatterns = [
     path('userform/', views.userform, name="userform"),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
